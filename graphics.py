c15945bd8ad7bb25ad230f914b829fc6184c8260
John McDonnell <quarry77@hotmail.com>
2026-03-09 00:01:23 -0700

    Made 3 different working screens and buttons for loading and clearing games

--- graphics.py
+++ graphics.py
@@ -20,20 +20,26 @@ class MainWindow(tk.Tk):
         # self.menu.pack(side="bottom", fill="both", expand=True)
 
         self.style = ttk.Style()
-        self.style.configure('Red.TFrame',background="red")
+        self.style.theme_use('clam')
+        self.style.configure('Red.TFrame',background="#FFB19B")
+        self.style.configure('Blue.TFrame',background="#C6C6FF")
+        self.style.configure('Green.TFrame',background="#E5F7E5")
 
-        self.header = Header(self, "Main Menu", True)
+        self.header = Header(self, "Main Menu", height=30)
         self.header.grid(row=0, column=0, sticky="nsew")
 
-        self.game_screen = tk.Frame(self, bg="blue")
-        self.game_screen.grid(row=1, column=0, sticky="nsew")
+        self.maker_screen = Maker(self, style="Blue.TFrame")
+        self.maker_screen.grid(row=1, column=0, sticky="nsew")
+
+        self.player_screen = Player(self, style="Green.TFrame")
+        self.player_screen.grid(row=1, column=0, sticky="nsew")
 
         self.menu = Menu(self, style="Red.TFrame")
         self.menu.grid(row=1, column=0, sticky="nsew")
 
 class Header(ttk.Frame):
     def __init__(self, parent, header_text="Header", back_enabled=False, *args, **kwargs):
-        super().__init__(parent, **kwargs)
+        super().__init__(parent, *args, **kwargs)
         self.parent = parent
         self.header_text = header_text
         self.back_enabled = back_enabled
@@ -41,28 +47,108 @@ class Header(ttk.Frame):
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=9)
+        self.grid_propagate(False)
 
-        if self.back_enabled:
-            self.back_btn = ttk.Button(text="<-- Back")
-            self.back_btn.grid(row=0, column=0, sticky="w")
+        
+        self.back_btn = ttk.Button(text="<-- Back", command=self.back)
+        self.back_btn.grid(row=0, column=0, sticky="w")
 
-        self.header_lbl = ttk.Label(self, text=self.header_text, font=("Helvetica", max(10, self.winfo_height() // 2)))
+        if not self.back_enabled:
+            self.back_btn.grid_remove()
+
+        self.header_lbl = ttk.Label(self, text=self.header_text, font=("Helvetica", max(10, self.winfo_height() // 2), "bold"))
         self.header_lbl.grid(row=0, column=0, columnspan=2, sticky="ns")
 
     def back(self):
-        pass
+        self.parent.menu.tkraise()
+        self.back_enabled = False
+        self.back_btn.grid_remove()
+        self.header_lbl.config(text="Main Menu")
 
 
 class Menu(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+        self.parent = parent
 
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=0)
-        self.rowconfigure(2, weight=1)
+        self.rowconfigure(2, weight=0)
+        self.rowconfigure(3, weight=1)
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=0)
-        self.columnconfigure(2, weight=1)
+        self.columnconfigure(2, weight=0)
+        self.columnconfigure(3, weight=0)
+        self.columnconfigure(4, weight=1)
 
-        self.new_game_btn = ttk.Button(self, text="New Game")
+        self.new_game_btn = ttk.Button(self, text="Load Game", command=self.load_maker)
         self.new_game_btn.grid(row=1, column=1)
+
+        self.new_game_btn = ttk.Button(self, text="Load Game", command=self.load_player)
+        self.new_game_btn.grid(row=2, column=1)
+
+        self.new_game_btn = ttk.Button(self, text="Maker", command=self.go_to_maker)
+        self.new_game_btn.grid(row=1, column=2)
+
+        self.new_game_btn = ttk.Button(self, text="Player", command=self.go_to_player)
+        self.new_game_btn.grid(row=2, column=2)
+
+        self.new_game_btn = ttk.Button(self, text="Clear Game", command=self.clear_maker)
+        self.new_game_btn.grid(row=1, column=3)
+
+        self.new_game_btn = ttk.Button(self, text="Clear Game", command=self.clear_player)
+        self.new_game_btn.grid(row=2, column=3)
+
+    def load_maker(self):
+        print("TODO: Load Maker")
+
+    def load_player(self):
+        print("TODO: Load Player")
+
+    def go_to_maker(self):
+        self.parent.maker_screen.tkraise()
+        self.parent.header.header_lbl.config(text="Sudoku Maker")
+        self.parent.header.back_enabled = True
+        self.parent.header.back_btn.grid()
+
+    def go_to_player(self):
+        self.parent.player_screen.tkraise()
+        self.parent.header.header_lbl.config(text="Sudoku Player")
+        self.parent.header.back_enabled = True
+        self.parent.header.back_btn.grid()
+
+    def clear_maker(self):
+        print("TODO: Clear Maker")
+
+    def clear_player(self):
+        print("TODO: Clear Player")
+
+class Maker(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=0)
+        self.rowconfigure(2, weight=0)
+        self.rowconfigure(3, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=0)
+        self.columnconfigure(2, weight=0)
+        self.columnconfigure(3, weight=0)
+        self.columnconfigure(4, weight=1)
+
+class Player(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.parent = parent
+
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=0)
+        self.rowconfigure(2, weight=0)
+        self.rowconfigure(3, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=0)
+        self.columnconfigure(2, weight=0)
+        self.columnconfigure(3, weight=0)
+        self.columnconfigure(4, weight=1)
